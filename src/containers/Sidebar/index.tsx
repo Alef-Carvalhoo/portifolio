ea37e6ecfb25c7fefbfa030b2c89dac98311b123
Alef Carvalho <alefcarvalho200310@gmail.com>
2024-05-28 15:16:12 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/Alef-Carvalhoo.png" />
+    <Titulo fontSize={20}>Alef Carvalho</Titulo>
+  </aside>
+)
+
+export default Sidebar
