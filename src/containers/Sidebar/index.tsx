237cb369e9daa1080f1c2c53e009d9d975e15224
Alef Carvalho <alefcarvalho200310@gmail.com>
2024-06-05 21:14:17 -0300

    estilizando a Sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,20 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Alef-Carvalhoo.png" />
-    <Titulo fontSize={20}>Alef Carvalho</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Alef Carvalho</Titulo>
+      <Paragrafo tipo="secundario">Alef-Carvalhoo</Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Front-end
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
